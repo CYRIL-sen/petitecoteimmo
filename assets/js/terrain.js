f92ae1856fc43baa87f0cdb995dfbd9e0553e12c
CYRIL-sen <join.seek@gmail.com>
2025-04-26 19:03:39 +0200

    Update terrain.js

--- assets/js/terrain.js
+++ assets/js/terrain.js
@@ -1,1 +1,37 @@
+let map;
+
+document.addEventListener('DOMContentLoaded', function () {
+  const urlParams = new URLSearchParams(window.location.search);
+  const terrainId = urlParams.get('id');
+
+  fetch('admin/annonces.json')
+    .then(response => response.json())
+    .then(data => {
+      const terrain = data.annonces[terrainId];
+
+      document.getElementById('titreTerrain').textContent = terrain.titre;
+      document.getElementById('localisationTerrain').innerHTML = `<strong>Localisation :</strong> ${terrain.localisation}`;
+      document.getElementById('superficieTerrain').innerHTML = `<strong>Superficie :</strong> ${terrain.superficie} m²`;
+      document.getElementById('prixTerrain').innerHTML = `<strong>Prix :</strong> ${terrain.prix} €`;
+
+      // Stocke coordonnées latitude et longitude
+      initMap(terrain.latitude, terrain.longitude);
+    })
+    .catch(error => {
+      console.error('Erreur chargement terrain :', error);
+    });
+});
+
+function initMap(lat = 14.745, lng = -17.475) {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: lat, lng: lng },
+    zoom: 14
+  });
+
+  new google.maps.Marker({
+    position: { lat: lat, lng: lng },
+    map,
+    title: "Localisation du terrain"
+  });
+}
 
